393e5252a2aef2f778305f52dded3390647d41be
SortingHyoek <68802641+SortingHyoek@users.noreply.github.com>
2020-09-14 23:00:16 +0900

    Add files via upload

---
+++ MainUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ButtonType
+{
+    New,
+    Continue,
+    Option,
+    Sound,
+    Back,
+    Quit
+}
+public class MainUI : MonoBehaviour
+{
+   
+}
