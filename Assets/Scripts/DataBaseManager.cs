a68891c0ea222f6b1580a6c9ad2d0d083922a138
doublejone831 <12161990@inha.edu>
2020-09-17 16:09:32 +0900

    파일정리

---
+++ Assets/Scripts/DataBaseManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataBaseManager : MonoBehaviour{
+
+    static public DataBaseManager instance;
+
+    public List<ItemData> ItemList = new List<ItemData> ();
+
+    public List<JournalData> JournalList = new List<JournalData>();
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+ 
+}
