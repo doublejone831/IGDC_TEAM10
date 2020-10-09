b056fc0a778ab232bacdc8709b923c8210c66ac5
SortingHyeok <balkans0502@hanmail.net>
2020-10-09 11:48:44 +0900

    20201009

---
+++ Assets/Scripts/MCScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MCScene : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject spawn_Point;
+    public bool isSpawn = true;
+
+    private void Awake()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        DontDestroyOnLoad(Player);
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawn_Point = GameObject.FindGameObjectWithTag("Respawn");
+        Player.transform.position = spawn_Point.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+   
+}
