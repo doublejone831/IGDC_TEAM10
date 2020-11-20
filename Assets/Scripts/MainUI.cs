44c040a01625ac7feb027f382b66cb935fcee724
Hideonbu <71164546+Hideonbu@users.noreply.github.com>
2020-11-20 11:15:28 +0900

    Update MainUI.cs

--- Assets/Scripts/MainUI.cs
+++ Assets/Scripts/MainUI.cs
@@ -2,16 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum ButtonType
-{
-    New,
-    Continue,
-    Option,
-    Sound,
-    Back,
-    Quit
-}
 public class MainUI : MonoBehaviour
 {
-   
+    public GameObject Menu;
+    public GameObject player;
+
+    void Start()
+    {
+        GameLoad();
+    }
+    void Update()
+    {
+        if (Menu.activeSelf)
+            Menu.SetActive(false);
+        else
+            Menu.SetActive(true);
+    }
+
+    public void Gamesave()
+    {
+        PlayerPrefs.SetFloat("PlayerX", player.transform.positon.x);
+        PlayerPrefs.SetFloat("PlayerY", player.transform.positon.y);
+        PlayerPrefs.SetFloat("QustId", questManager.questId);
+        PlayerPrefsSave();
+
+        Menu.SetActive(false);
+    }
+
+    public void GameLoad()
+    {
+        if (!PlayerPrefs.HasKey("Player X"))
+            return 0;
+
+        float x = PlayerPrefs.GetFloat("Player X");
+        float x = PlayerPrefs.GetFloat("Player Y");
+        int questId = PlayerPrefs.GetInt("QustId");
+        int questActionIndex = PlayerPrefs.GetInt("QustActionIndex");
+
+        player.transform.position = new Vector3(x, y, 0);
+        questManager.questId = questId;
+        questManager.questActionIndex = questActionIndex;
+    }
 }
+
